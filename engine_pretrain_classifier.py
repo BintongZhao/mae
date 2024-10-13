ad4a439278ac772e17ecb4b9afe7b60708cb4c16
xinglibao <xinglibao383@gmail.com>
2024-10-13 22:30:43 +0800

    func: 实现使用mae的encoder作为特征提取器，训练分类器

---
+++ engine_pretrain_classifier.py
@@ -0,0 +1,81 @@
+import os
+import torch
+from torch import nn
+from util.accumulator import Accumulator
+
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = torch.argmax(y_hat, dim=1)
+    if len(y.shape) > 1 and y.shape[1] > 1:
+        y = torch.argmax(y, dim=1)
+    cmp = y_hat == y
+    return float(cmp.type(y.dtype).sum()) / y.shape[0]
+
+
+def evaluate(net, feature_extractor, data_iter, loss_func):
+    # 验证损失之和, 正确预测的样本数, 样本数
+    metric = Accumulator(3)
+    device = next(iter(net.parameters())).device
+    net.eval()
+    with torch.no_grad():
+        for x, y in data_iter:
+            x = feature_extractor.extract(x)
+            x, y = x.to(device), y.to(device)
+            y_hat = net(x)
+            metric.add(loss_func(y_hat, y).item() * y.shape[0], accuracy(y_hat, y) * y.shape[0], y.shape[0])
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train(net, feature_extractor, train_iter, val_iter, learning_rate, num_epochs, patience, devices, checkpoint_save_dir_path, logger):
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+
+    net.apply(init_weights)
+
+    # 在多个GPU上并行训练模型
+    # net = nn.DataParallel(net, device_ids=devices).to(devices[0])
+    net = net.to(devices[0])
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+    loss_func = nn.CrossEntropyLoss()
+
+    best_state_dict = net.state_dict()
+    min_val_loss = float('inf')
+    min_val_loss_epoch = 0
+    current_patience = 0
+
+    for epoch in range(num_epochs):
+        # 训练损失之和, 正确预测的样本数, 样本数
+        metric = Accumulator(3)
+        net.train()
+        for i, (x, y) in enumerate(train_iter):
+            optimizer.zero_grad()
+            x = feature_extractor.extract(x)
+            x, y = x.to(devices[0]), y.to(devices[0])
+            y_hat = net(x)
+            loss = loss_func(y_hat, y)
+            loss.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(loss * y.shape[0], accuracy(y_hat, y) * y.shape[0], y.shape[0])
+        train_loss, train_acc = metric[0] / metric[2], metric[1] / metric[2]
+        val_loss, val_acc = evaluate(net, feature_extractor, val_iter, loss_func)
+        logger.record(
+            f'Epoch: {epoch}, current patience: {current_patience + 1}, train loss: {train_loss:.3f}, train acc: {train_acc:.3f}, val loss: {val_loss:.3f}, val acc: {val_acc:.3f}')
+        if (epoch + 1) % 5 == 0 or (epoch + 1) == num_epochs:
+            checkpoint_save_path = os.path.join(checkpoint_save_dir_path, f"checkpoint-{epoch}.pth")
+            torch.save(net.state_dict(), checkpoint_save_path)
+        if val_loss < min_val_loss:
+            best_state_dict = net.state_dict()
+            min_val_loss = val_loss
+            min_val_loss_epoch = epoch
+            current_patience = 0
+        else:
+            current_patience += 1
+            if current_patience >= patience:
+                logger.record(f'Early stopping after {epoch + 1} epochs')
+                break
+
+    torch.save(best_state_dict, os.path.join(checkpoint_save_dir_path, "best_state_dict.pth"))
+    logger.record(f"The best testing loss occurred in the {min_val_loss_epoch} epoch")
---
+++ main_pretrain_classifier.py
@@ -0,0 +1,78 @@
+import os
+import datetime
+import torch
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+from util.wifi_dataset import WiFiDataset
+from util.crop_for_wifi import CropForWiFi
+from util.mae_feature_extractor import MAEFeatureExtractor
+from util.logger import Logger
+from models.mlp_classifier import MLPClassifier
+from engine_pretrain_classifier import train
+
+
+def prepare_mae_feature_extractor(model_name, checkpoint_path, device=None):
+    mae_feature_extractor = MAEFeatureExtractor(model_name, checkpoint_path, device)
+    return mae_feature_extractor
+
+
+def prepare_mlp_classifier(input_shape, num_classes, hidden_dim=1024, dropout=0.5, device=None):
+    return MLPClassifier(input_shape, num_classes, hidden_dim, dropout, device)
+
+
+def prepare_dataloader(mode='train', batch_size=4, num_workers=4):
+    data_path = os.path.join('../', '../', 'data', 'wifi-dataset')
+    transform_train = transforms.Compose([
+        CropForWiFi(),
+        transforms.ToTensor()])
+    dataset_train = WiFiDataset(root_dir=data_path, mode=mode, transform=transform_train)
+    shuffle = True if mode == 'train' else False
+    return DataLoader(dataset_train, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+
+def main():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    output_save_path = os.path.join('./', 'output_dir', timestamp)
+
+    logger = Logger(save_path=output_save_path)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
+
+    dataloader_train = prepare_dataloader(mode='train')
+    dataloader_val = prepare_dataloader(mode='val')
+
+    mae_model_name = 'mae_vit_base_patch4'
+    mae_checkpoint_path = './output_dir/20241012193444/checkpoint-49.pth'
+    mae_feature_extractor = prepare_mae_feature_extractor(mae_model_name, mae_checkpoint_path, device)
+
+    input_shape = (128, 768)
+    num_classes = 3
+    mlp_classifier = prepare_mlp_classifier(input_shape, num_classes, device=device)
+
+    train(mlp_classifier, mae_feature_extractor, dataloader_train, dataloader_val,
+          0.0001, 50, 5, [device], output_save_path, logger)
+
+
+def test():
+    mae_model_name = 'mae_vit_base_patch4'
+    mae_checkpoint_path = './output_dir/20241012193444/checkpoint-49.pth'
+    input_shape = (128, 768)
+    num_classes = 10
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    mae_feature_extractor = prepare_mae_feature_extractor(mae_model_name, mae_checkpoint_path, device)
+    mlp_classifier = prepare_mlp_classifier(input_shape, num_classes, device=device)
+    input_tensor = torch.randn(64, 1, 128, 16).to(device)
+
+    features = mae_feature_extractor.extract(input_tensor)
+    mlp_classifier.eval()
+    with torch.no_grad():
+        outputs = mlp_classifier(features)
+
+    print(f'Extracted features shape: {features.shape}')
+    print(f'Output shape: {outputs.shape}')
+
+
+if __name__ == '__main__':
+    main()
